2cd5ddca0ea40e91bb64fd56ce5f4dbe08252b6f
xsha256 <43494720+xsha256@users.noreply.github.com>
2023-11-12 21:19:02 +0100

    simple GUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAnalysisButtonClick = () => {
+    // Placeholder for image analysis logic
+    console.log(`Analyzing image with URL: ${inputValue}`);
+  };
+
+  const handleGenerationButtonClick = () => {
+    // Placeholder for image generation logic
+    console.log(`Generating image with prompt: ${inputValue}`);
+  };
+
+  return (
+    <div>
+      <h1>Image Analysis and Generation App</h1>
+      <label>
+        Enter Image URL or Prompt:
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+      </label>
+      <br />
+      <button onClick={handleAnalysisButtonClick}>Analyse Image</button>
+      <button onClick={handleGenerationButtonClick}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
